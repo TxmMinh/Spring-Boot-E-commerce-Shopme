e785347bd693d4442149a97c1f513404606990e1
TxmMinh <xuanminh2107@gmail.com>
2023-07-29 00:44:50 +0700

    Code Manage States/Provinces

---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/states_setting.js
@@ -0,0 +1,181 @@
+var buttonLoad4States;
+var dropDownCountry4States;
+var dropDownStates;
+var buttonAddState;
+var buttonUpdateState;
+var buttonDeleteState;
+var labelStateName;
+var fieldStateName;
+
+$(document).ready(function () {
+    buttonLoad4States = $("#buttonLoadCountriesForStates");
+    dropDownCountry4States = $("#dropDownCountriesForStates");
+    dropDownStates = $("#dropDownStates");
+    buttonAddState = $("#buttonAddState");
+    buttonUpdateState = $("#buttonUpdateState");
+    buttonDeleteState = $("#buttonDeleteState");
+    labelStateName = $("#labelStateName");
+    fieldStateName = $("#fieldStateName");
+
+    buttonLoad4States.click(function () {
+        loadCountries4States();
+    });
+
+    dropDownCountry4States.on("change", function () {
+       loadStates4Country();
+    });
+
+    dropDownStates.on("change", function () {
+        changeFormStateToSelectedStates();
+    });
+
+    buttonAddState.click(function () {
+        if (buttonAddState.val() == "Add") {
+            addState();
+        } else {
+            changeFormStateToNew();
+        }
+    });
+
+    buttonUpdateState.click(function () {
+        updateStates();
+    });
+
+    buttonDeleteState.click(function () {
+        deleteStates();
+    });
+});
+
+function loadCountries4States() {
+    url = contextPath + "countries/list";
+
+    $.get(url, function (responseJSON) {
+        dropDownCountry4States.empty();
+
+        $.each(responseJSON, function (index, country) {
+            $("<option>").val(country.id).text(country.name).appendTo(dropDownCountry4States);
+        });
+    }).done(function () {
+        buttonLoad4States.val("Refresh Country List");
+        showToastMessage("All countries have been loaded");
+    }).fail(function () {
+        showToastMessage("ERROR: Could not connect to server or server encountered an error");
+    });
+}
+
+function loadStates4Country() {
+    selectedCountry = $("#dropDownCountriesForStates option:selected");
+    countryId = selectedCountry.val();
+    url = contextPath + "states/list_by_country/" + countryId;
+
+    $.get(url, function (responseJSON) {
+       dropDownStates.empty();
+
+       $.each(responseJSON, function (index, state) {
+          $("<option>").val(state.id).text(state.name).appendTo(dropDownStates);
+       });
+    }).done(function () {
+        changeFormStateToNew();
+        showToastMessage("All states have been loaded for country " + selectedCountry.text());
+    }).fail(function () {
+        showToastMessage("ERROR: Could not connect to server or server encountered an error");
+    });
+}
+
+function changeFormStateToSelectedStates() {
+    buttonAddState.prop("value", "New");
+    buttonUpdateState.prop("disabled", false);
+    buttonDeleteState.prop("disabled", false);
+
+    labelStateName.text("Selected State/Province:");
+
+    selectedStateName = $("#dropDownStates option:selected").text();
+    fieldStateName.val(selectedStateName);
+}
+
+function addState() {
+    url = contextPath + "states/save";
+    stateName = fieldStateName.val();
+
+    selectedCountry = $("#dropDownCountriesForStates option:selected");
+    countryId = selectedCountry.val();
+    countryName = selectedCountry.text();
+
+    jsonData = {name: stateName, country: {id: countryId, name: countryName}};
+
+    $.ajax({
+       type: 'POST',
+       url: url,
+       beforeSend: function (xhr) {
+           xhr.setRequestHeader(csrfHeaderName, csrfValue);
+       },
+       data: JSON.stringify(jsonData),
+       contentType: 'application/json'
+    }).done(function (stateId) {
+        selectNewlyAddedState(stateId, stateName);
+        showToastMessage("All states have been loaded for country " + selectedCountry.text());
+    }).fail(function () {
+        showToastMessage("ERROR: Could not connect to server or server encountered an error");
+    });
+}
+
+function changeFormStateToNew() {
+    buttonAddState.val("Add");
+    labelStateName.text("State/Province Name:");
+
+    buttonUpdateState.prop("disabled", true);
+    buttonDeleteState.prop("disabled", true);
+
+    fieldStateName.val("").focus();
+}
+
+function selectNewlyAddedState(stateId, stateName) {
+    $("<option>").val(stateId).text(stateName).appendTo(dropDownStates);
+
+    $("#dropDownStates option[value='" + stateId + "']").prop("selected", true);
+
+    fieldStateName.val("").focus();
+}
+
+function updateStates() {
+    url = contextPath + "states/save";
+    stateId = dropDownStates.val();
+    stateName = fieldStateName.val();
+
+    selectedCountry = $("#dropDownCountriesForStates option:selected");
+    countryId = selectedCountry.val();
+    countryName = selectedCountry.text();
+
+    jsonData = {id: stateId, name: stateName, country: {id: countryId, name: countryName}};
+
+    $.ajax({
+        type: 'POST',
+        url: url,
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader(csrfHeaderName, csrfValue);
+        },
+        data: JSON.stringify(jsonData),
+        contentType: 'application/json'
+    }).done(function (stateId) {
+        $("#dropDownStates option:selected").text(stateName);
+        showToastMessage("The state has been updated");
+    }).fail(function () {
+        showToastMessage("ERROR: Could not connect to server or server encountered an error");
+    });
+}
+
+function deleteStates() {
+    stateId = dropDownStates.val();
+
+    url = contextPath + "states/delete/" + stateId;
+    $.get(url, function () {
+        $("#dropDownStates option[value='" + stateId + "']").remove();
+        changeFormStateToNew()
+    }).done(function () {
+        showToastMessage("The state has been deleted");
+    }).fail(function () {
+        showToastMessage("ERROR: Could not connect to server or server encountered an error");
+    });
+}
+
+
