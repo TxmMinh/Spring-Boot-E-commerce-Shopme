34d01073f92dc6acdf9fcb404aeac58e4242f7d0
TxmMinh <xuanminh2107@gmail.com>
2023-08-09 17:21:52 +0700

    Code Add Product to Shipping Cart

---
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/CartItem.java
@@ -0,0 +1,61 @@
+package com.shopme.common.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+    private int quantity;
+
+    public CartItem() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "CartItem [id=" + id + ", customer=" + customer.getFullName() + ", product=" + product.getShortName()
+                + ", quantity=" + quantity + ']';
+    }
+}
