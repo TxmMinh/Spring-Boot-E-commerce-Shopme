58f02dfef40c37eea7918382ddb537ee9107ca36
TxmMinh <xuanminh2107@gmail.com>
2023-08-10 01:08:17 +0700

    Code Remove Product from Shopping Cart

--- ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/shoppingcart/ShoppingCartService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/shoppingcart/ShoppingCartService.java
@@ -57,4 +57,8 @@ public class ShoppingCartService {
 
         return subtotal;
     }
+
+    public void removeProduct(Integer productId, Customer customer) {
+        cartRepo.deleteByCustomerAndProduct(customer.getId(), productId);
+    }
 }
