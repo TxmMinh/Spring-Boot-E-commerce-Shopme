7782bad90b98dd1e5ba941a8cca7fed2409e70e4
TxmMinh <xuanminh2107@gmail.com>
2023-08-24 14:23:40 +0700

    Code Return Order Function

---
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/order/OrderReturnResponse.java
@@ -0,0 +1,12 @@
+package com.shopme.order;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderReturnResponse {
+    private Integer id;
+}
